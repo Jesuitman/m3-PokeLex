27f8d56f2640fb9c561560248a2dc0887e71cbb0
Alexandre Hastings <alex.hastings.1227@gmail.com>
2024-01-11 13:34:28 -0500

    add in team display

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import List from '../List/List';
 import PokemonCard from '../PokemonCard/PokemonCard';
+import TeamDisplay from '../TeamDisplay/TeamDisplay'; // Import the TeamDisplay component
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [team, setTeam] = useState(Array(6).fill(null));
 
   const handlePokemonClick = (pokemonDetails) => {
     setSelectedPokemon(pokemonDetails);
@@ -14,19 +16,49 @@ function App() {
     setSelectedPokemon(null);
   };
 
+  const handleAddToTeam = () => {
+    if (selectedPokemon) {
+      const updatedTeam = [...team];
+      const emptySlotIndex = updatedTeam.findIndex(pokemon => pokemon === null);
+      if (emptySlotIndex !== -1) {
+        updatedTeam[emptySlotIndex] = selectedPokemon;
+        setTeam(updatedTeam);
+        console.log(team)
+      } else {
+        console.log("Your team is full. Remove a Pokemon to add a new one.");
+      }
+    }
+  };
+
+  const handleRemoveFromTeam = (index) => {
+    const updatedTeam = [...team];
+    updatedTeam[index] = null;
+    setTeam(updatedTeam);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>PokéLex</h1>
+        <button className='gen-button'>Kanto</button>
+        <button className='gen-button'>Johto</button>
+        <button className='gen-button'>Hoenn</button>
+        <button className='gen-button'>Sinnoh</button>
+        <button className='gen-button'>Unova</button>
+        <button className='gen-button'>Kalos</button>
+        <button className='gen-button'>Alola</button>
+        <button className='gen-button'>Galar</button>
+        <button className='gen-button'>Paldea</button>
       </header>
       <div className="main-container">
         <div className="pokemon-details">
-          <h2></h2>
-          {selectedPokemon && <PokemonCard pokemon={selectedPokemon} onClose={handleCloseCard} />}
+          {selectedPokemon && (
+            <PokemonCard pokemon={selectedPokemon} onClose={handleCloseCard} handleAddToTeam={() => handleAddToTeam(selectedPokemon)} /> )}
         </div>
         <div className="list-container">
           <List onPokemonClick={handlePokemonClick} />
         </div>
+        <TeamDisplay team={team} handleRemoveFromTeam={handleRemoveFromTeam} />
       </div>
     </div>
   );
--- src/PokemonCard/PokemonCard.js
+++ src/PokemonCard/PokemonCard.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import './PokemonCard.css';
 
-function PokemonCard({ pokemon, onClose }) {
-
+function PokemonCard({ pokemon, onClose, handleAddToTeam }) {
   const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
+  const handleClose = () => {
+    console.log('Closing card...'); // Add this log for debugging
+    onClose();
+  };
+
+  const handleAdd = () => {
+    console.log('Adding to team...');
+    handleAddToTeam(); // Pass the selectedPokemon to the handleAddToTeam function
+  };
+
   return (
     <div className="pokemon-details">
-      {pokemon ? (
-        <>
-          <div className="sprite-container">
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-sprite" />
-          </div>
-          <h2>{capitalizeFirstLetter(pokemon.name)}</h2>
-          <p>Type: {pokemon.types.map(type => capitalizeFirstLetter(type.type.name)).join(', ')}</p>
-          <p>Abilities:</p>
-          <ul>
-            {pokemon.abilities.map((ability, index) => (
-              <li key={index}>{capitalizeFirstLetter(ability.ability.name)}</li>
-            ))}
-          </ul>
-          <button onClick={() => console.log("Add to team")}>
-            Add to Team
-          </button> 
-          <button className="close-button" onClick={onClose}>
-            Close
-          </button>
-        </>
-      ) : (
-        <p>Click a Pokémon to see details</p>
-      )}
+      <div className="sprite-container">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-sprite" />
+      </div>
+      <h2>{capitalizeFirstLetter(pokemon.name)}</h2>
+      <p>Type: {pokemon.types.map(type => capitalizeFirstLetter(type.type.name)).join(', ')}</p>
+      <p>Abilities:</p>
+      <ul>
+        {pokemon.abilities.map((ability, index) => (
+          <li key={index}>{capitalizeFirstLetter(ability.ability.name)}</li>
+        ))}
+      </ul>
+      <button className="gen-button" onClick={handleAdd}>Add to Team</button>
+      <button className="gen-button" onClick={handleClose}>Close</button>
     </div>
   );
 }
 
 export default PokemonCard;
-
